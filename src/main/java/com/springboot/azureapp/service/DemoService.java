3d050a9ec0fe8907486e4efc2fe189ab1bbbd4e9
Ashish Singh <ash.singh112233@gmail.com>
2024-10-15 22:35:45 +0530

    changes

--- src/main/java/com/springboot/azureapp/service/DemoService.java
+++ src/main/java/com/springboot/azureapp/service/DemoService.java
@@ -11,7 +11,6 @@ import java.util.List;
 public interface DemoService {
 
     DemoModel addEntries(DemoModel demoModel);
-
     
     DemoModel getEntries(Long id) throws DemoModelNotFoundException;
 
