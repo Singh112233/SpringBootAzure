d1f27d1a3a129913c05b5b2b726122532e847d9c
Ashish Singh <ash.singh112233@gmail.com>
2024-10-15 21:40:22 +0530

    push

--- src/main/java/com/springboot/azureapp/service/DemoService.java
+++ src/main/java/com/springboot/azureapp/service/DemoService.java
@@ -12,6 +12,7 @@ public interface DemoService {
 
     DemoModel addEntries(DemoModel demoModel);
 
+    
     DemoModel getEntries(Long id) throws DemoModelNotFoundException;
 
     List<DemoModel> getAllEntries();
